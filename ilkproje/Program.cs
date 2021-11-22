08190dea5073f1b087cf45d2763736de15adb430
mbgdev <faraziakademi@gmail.com>
2021-11-22 13:41:14 +0300

    Proje dosyası ekle.

---
+++ ilkproje/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace ilkproje
+{
+    class ilkproje
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int tahmin = 0, tutulan, sayac = 0;
+            Random rasgele = new Random();
+            tutulan = rasgele.Next(1, 100);
+            while (tahmin != tutulan)
+            {
+                Console.WriteLine(" ");
+                sayac++;
+                Console.Write("Sayı giriniz ");
+                tahmin = rasgele.Next(1, 100);
+                Console.WriteLine(tahmin);
+
+
+                if (tahmin > tutulan)
+                {
+
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Sayıyı küçült");
+
+                }
+                else if (tahmin < tutulan)
+                {
+
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Sayıyı büyüt");
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Tebrikler.");
+            Console.WriteLine(" ");
+            Console.WriteLine("{0}.hakkınızda bildiniz", sayac);
+            Console.ReadKey();
+
+
+        }
+
+
+
+
+    }
+
+}
+
+
